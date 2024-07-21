9bc4e325508efedd42bdcfeba94de2a6a265b632
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-07-21 19:23:32 -0300

    C1 - Criando o Projeto Lista de Tarefas.

---
+++ santander-bootcamp-dio-2024/src/test/java/me/santander/dio/SantanderBootcampDio2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.santander.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampDio2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
